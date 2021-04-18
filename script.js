c1174f651b2a5c57b908b45b019629e8d2ff32f8
Emmanuel <pacesetter4lyf@gmail.com>
2021-04-18 07:56:39 +0100

    modified the logs error message

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const fs = require('fs');
 fetch('http://jsonplaceholder.typicode.com/posts')
     // Handle success
     .then(response => (response.json()) ) // convert to json
-    .then(json => JSON.stringify(json))    
+    .then(json => JSON.stringify(json))   //stringify json to text
     .then(data => {
         fs.writeFile('./result/posts.json', data, function (err) {
-            if (err) throw err;
+            if (err) {
+                consloe.log("AN ERROR OCCURED")
+                throw err;
+            }
             console.log('DATA WRITTEN!');
         })
     })
